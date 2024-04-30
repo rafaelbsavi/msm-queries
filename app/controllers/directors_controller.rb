49ec7b72131aac4d357cd40991f6d135265252af
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-04-30 16:35:46 +0000

    directors details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,16 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "directors_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_record = Director.where({ :id => the_id})
+
+    @the_director = matching_record.at(0)
+
+
+
+    render({ :template => "directors_templates/details"})
+  end
 end
