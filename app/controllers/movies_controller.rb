9dab2d560527c8d2e22815aca26c53899724b9b5
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-04-30 17:48:13 +0000

    movies - list and details

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+  def index
+    render({ :template => "movies_templates/list"})
+  end
+  
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_record = Movie.where({ :id => the_id})
+
+    @the_movie = matching_record.at(0)
+
+    render({ :template => "movies_templates/details"})
+  end
+
+
+
+end
